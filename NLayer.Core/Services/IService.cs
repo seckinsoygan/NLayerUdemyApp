6ee8a40e8cfe1fa4356a12a28e5ff781c731aa93
seckinsoygan <seckinsoygann@gmail.com>
2022-10-31 01:37:22 +0300

    v1.1

--- NLayer.Core/Services/IService.cs
+++ NLayer.Core/Services/IService.cs
@@ -8,7 +8,7 @@ namespace NLayer.Core.Services
         Task<IEnumerable<T>> GetAllAsync();
         IQueryable<T> Where(Expression<Func<T, bool>> expression);
         Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
-        Task AddAsync(T entity);
+        Task<T> AddAsync(T entity);
         Task AddRangeAsync(IEnumerable<T> entities);
         Task UpdateAsync(T entity);
         Task RemoveAsync(T entity);
---
+++ NLayer.Service/Services/Service.cs
@@ -0,0 +1,79 @@
+﻿using NLayer.Core.Repositories;
+using NLayer.Core.Services;
+using NLayer.Repository.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Service.Services
+{
+    public class Service<T> : IService<T> where T:class
+    {
+        private readonly IGenericRepository<T> _repository;
+        private readonly UnitOfWork _unitOfWork;
+
+        public Service(IGenericRepository<T> repository, UnitOfWork unitOfWork)
+        {
+            _repository = repository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await _repository.AddAsync(entity);
+            await _unitOfWork.CommitAsync();
+            return entity;
+        }
+
+        public async Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities)
+        {
+            await _repository.AddRangeAsync(entities);
+            await _unitOfWork.CommitAsync();
+            return entities;
+        }
+
+        public Task<bool> AnyAsync(System.Linq.Expressions.Expression<Func<T, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Service<T> service &&
+                   EqualityComparer<IGenericRepository<T>>.Default.Equals(_repository, service._repository) &&
+                   EqualityComparer<UnitOfWork>.Default.Equals(_unitOfWork, service._unitOfWork);
+        }
+
+        public Task<IEnumerable<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveRangeAsync(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> Where(System.Linq.Expressions.Expression<Func<T, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
